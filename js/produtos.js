86fef923614f5d3ebc2598e3ff454e86fa92890d
= <=>
2022-04-29 00:08:16 -0300

    commit_inicial

---
+++ js/produtos.js
@@ -0,0 +1,30 @@
+function exibirCategoria(categoria){
+  alert(categoria);
+
+  let elementos = document.getElementsByClassName("box_produto");
+  console.log(elementos);
+  for(var i=0; i<elementos.length; i++){
+    console.log(elementos[i].id);
+    if(categoria == elementos[i].id)
+      elementos[i].style = "display:block"; 
+    else
+      elementos[i].style = "display:none";
+  }
+}
+
+let exibirTodos = () =>{
+  elementos = document.getElementsByClassName("box_produto");
+  console.log(elementos);
+  for(let i=0; i<elementos.length; i++){
+    elementos[i].style = "display:block";
+  }
+}
+
+let destaque = (imagem) =>{
+  console.log(imagem);
+  if(imagem.width == 240){
+    imagem.width = 120; 
+  }else{
+    imagem.width = 240;
+  }
+}
